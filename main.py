4702f42808314110421d9b9ee50ae06992b21e5b
Mupaky <georgi.jelev94@gmail.com>
2024-11-01 07:57:31 +0000

    Another test

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from controllers.AppController import AppController
 
 if __name__ == "__main__":
     # Added this very important line
+    # Adding second line for test purpose
     app = AppController()
     app.mainloop()
